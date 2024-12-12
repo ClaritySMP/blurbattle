7417c260da0873d801a560528144dd3d7255d35c
NEOLabs-software <THAYEEBOI890@GMAIL.COM>
2024-12-11 18:46:35 -0600

    quick eye

--- src/main/java/org/neo/blurbattle/Blurbattle.java
+++ src/main/java/org/neo/blurbattle/Blurbattle.java
@@ -36,6 +36,30 @@ public final class Blurbattle extends JavaPlugin implements Listener {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
         getCommand("blurbattle").setExecutor(this);
+        String eyeAscii =
+                """                                
+                                 
+                                 ...',;;:cccccccc:;,..
+                            ..,;:cccc::::ccccclloooolc;'.
+                         .',;:::;;;;:loodxk0kkxxkxxdocccc;;'..
+                       .,;;;,,;:coxldKNWWWMMMMWNNWWNNKkdolcccc:,.
+                    .',;;,',;lxo:...dXWMMMMMMMMNkloOXNNNX0koc:coo;.
+                 ..,;:;,,,:ldl'   .kWMMMWXXNWMMMMXd..':d0XWWN0d:;lkd,
+               ..,;;,,'':loc.     lKMMMNl. .c0KNWNK:  ..';lx00X0l,cxo,.
+             ..''....'cooc.       c0NMMX;   .l0XWN0;       ,ddx00occl:.
+           ..'..  .':odc.         .x0KKKkolcld000xc.       .cxxxkkdl:,..
+         ..''..   ;dxolc;'         .lxx000kkxx00kc.      .;looolllol:'..
+        ..'..    .':lloolc:,..       'lxkkkkk0kd,   ..':clc:::;,,;:;,'..
+        ......   ....',;;;:ccc::;;,''',:loddol:,,;:clllolc:;;,'........
+            .     ....'''',,,;;:cccccclllloooollllccc:c:::;,'..
+                    .......'',,,,,,,,;;::::ccccc::::;;;,,''...
+                      ...............''',,,;;;,,''''''......
+                           ............................""";
+                        
+
+        getLogger().info(ChatColor.GREEN + eyeAscii);
+        getLogger().info(ChatColor.GREEN + "Blurbattle plugin has been enabled!");
+
     }
 
     @Override
